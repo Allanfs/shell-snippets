c88f854e9c7ae23bfa5d7dcc80b0b01f2943e71e
ALLAN SILVA <allan.silva.dev@gmail.com>
2023-03-19 18:15:12 -0300

    add ammend and fixup commands

---
+++ ammend.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git diff --name-only | wc -l | grep -q 0 || {
+    echo "Nenhum arquivo preparado para commit."
+    echo "execute: git add <arquivo>"
+    exit 1
+}
+
+git commit --amend --no-edit
