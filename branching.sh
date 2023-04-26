91af13d32c40e66cdbfff039e5a02d713fdcd6ef
ALLAN SILVA <allan.silva.dev@gmail.com>
2023-04-26 16:57:10 -0300

    add script de branching

---
+++ branching.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+generate_branch_name() {
+    bname=`echo $1 | tr [:lower:] [:upper:]`
+    echo ${bname}--`git branch --show-current`
+}
+
+# $1 : branch de destino (dev, hml, stg)
+checkout_to() {
+    git branch --show-current | read CURRENT_BRANCH
+    NEXT_BRANCH=`generate_branch_name $1`
+    git checkout $NEXT_BRANCH
+    if [[ $? -eq 1 ]]; then
+        git checkout -b $NEXT_BRANCH
+    fi
+    store_previous_branch $CURRENT_BRANCH
+}
+
+checkout_back() {
+    git branch --show-current | read CURRENT
+    branch_back=`get_previous_branch`
+    store_previous_branch $CURRENT
+    git checkout $branch_back
+}
+
+get_previous_branch() {
+    cat $TMP_FILE
+}
+TMP_FILE="/tmp/branching-previous-branch"
+store_previous_branch() {
+    echo $1 > $TMP_FILE
+}
+
+# $1 : branch que será integrada (dev, hml, stg)
+merge_to() {
+    CURRENT_BRANCH=`git branch --show-current`
+
+    checkout_to $1
+
+    git merge $CURRENT_BRANCH
+
+    checkout_back
+}
